7064d4c907679f1b24ed07b3b7041e79d9b012d6
idzharbae <idzharbae@gmail.com>
2019-10-28 17:41:41 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button} from 'react-bootstrap';
+import Navigation from './main_layout/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Button variant="danger">Danger !</Button>
       </header>
     </div>
+  );
+}
+
+function Link(){
+  return (
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
   );
 }
 
