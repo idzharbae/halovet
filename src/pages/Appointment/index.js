213b9492396c91bd686be79be8c7f8a326f61011
idzharbae <idzharbae@gmail.com>
2019-11-15 12:59:28 +0700

    appointment

--- src/pages/Appointment/index.js
+++ src/pages/Appointment/index.js
@@ -3,6 +3,7 @@ import PageTemplate from '../page_template';
 import axios from 'axios';
 import queryString from 'query-string';
 import View from './view';
+import { getCookie } from '../../helper/cookies';
 
 class Appointment extends PageTemplate{
     constructor(props){
@@ -12,12 +13,32 @@ class Appointment extends PageTemplate{
                 submitForm = {this.submitForm}
                 bindForm = {this.bindForm}
                 formGroup = {this.formGroup}
-            />)
+            />),
+            doctor_name: '',
+            pet_name: '',
+            complaint: '',
+            time: null
         }
     }
 
     submitForm = (e) => {
-        
+        e.preventDefault()
+        const { doctor_name, pet_name, complaint, time } = this.state;
+        const token = getCookie('jwt');
+        let config = {
+            headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Bearer '+token
+            }
+        };
+        const data = queryString.stringify({ doctor_name, pet_name, complaint, time }).replace(/%20/g,'+');
+        axios.post('http://0.0.0.0:8000/appointment', data, config)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((e) => {
+                console.log(e.response);
+            });
     }
 
 }
