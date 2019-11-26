9f9d8ee0c8440e1db8881bcaf15e1f44017ade03
idzharbae <idzharbae@gmail.com>
2019-11-26 16:23:23 +0700

    Forum get

--- src/pages/Forum/index.js
+++ src/pages/Forum/index.js
@@ -5,6 +5,7 @@ import queryString from 'query-string';
 import { getCookie } from '../../helper/cookies';
 import React from 'react';
 import View from './view';
+import { tsImportEqualsDeclaration } from '@babel/types';
 
 class Forum extends PageTemplate{
   constructor(props){
@@ -13,28 +14,47 @@ class Forum extends PageTemplate{
       title: '',
       category: '',
       content: '',
-      view: <View 
-        submitForm = {this.submitForm}
-        formGroup = {this.formGroup}
-        bindForm = {this.bindForm}
-      />
+      view: <div></div>,
+      config: {
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer '+ getCookie('jwt')
+        }
+      }
     };
+  }
+  componentDidMount(){
+    axios.get('http://0.0.0.0:8000/forums?limitstart=0&limit=5', this.state.config)
+      .then((result) => {
+        const response = result.data;
+        if(response.Status){
+          console.log(response);
+          this.setState({
+            view: <View 
+            submitForm = {this.submitForm}
+            formGroup = {this.formGroup}
+            bindForm = {this.bindForm}
+            formSelection = {this.formSelection}
+            topics = {response.Data.Appointments}
+          />
+          })
+        }
+        else{
+          console.log(result);
+        }
+      })
+      .catch((e) => {
+
+      })
   }
   submitForm = (e) => {
     e.preventDefault()
     const { title, category, content } = this.state;
-    const token = getCookie('jwt');
-    let config = {
-        headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Bearer '+token
-        }
-    };
     const data = queryString.stringify({ title, category, content }).replace(/%20/g,'+');
     console.log(data);
-    axios.post('http://0.0.0.0:8000/forum', data, config)
+    axios.post('http://0.0.0.0:8000/forum', data, this.state.config)
         .then((result) => {
-        const response = result.data;
+        const response = result;
         if(response.Status === true){
           this.props.addAlert('Berhasil menambah post', "success");
         }
--- src/pages_admin/index.js
+++ src/pages_admin/index.js
@@ -1,33 +1,66 @@
-import React from 'react';
-import { Navigation, Footer } from '../main_layout';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
-import * as Pages from './pages';
-class Admin extends React.Component{
-  constructor(props){
-    super(props);
-  }
+import React, { Component } from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import { Button, Nav as BootstrapNav, Card, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class Admin extends Component {
 
-  render(){
+  render() {
     return (
-      
-      <Router>
-        <div className="Admin">
-          <header className="App-header">
-          <Switch>
-            <Route exact path='/' component={Pages.Home}  />
-          </Switch>
-          </header>
-          <Footer />
-        </div>
-      </Router>
+      <div className="Admin">
+        <SideNav
+          onSelect={(selected) => {
+              // Add your code here
+          }}
+          >
+          <SideNav.Toggle />
+          <SideNav.Nav defaultSelected="home">
+              <NavItem eventKey="home">
+                  <NavIcon>
+                      <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                  </NavIcon>
+                  <NavText>
+                      Home
+                  </NavText>
+              </NavItem>
+              <NavItem eventKey="appointment">
+                  <NavIcon>
+                      <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                  </NavIcon>
+                  <NavText>
+                      Appointments
+                  </NavText>
+              </NavItem>
+          </SideNav.Nav>
+      </SideNav>
+      <BootstrapNav className="justify-content-end">
+        <BootstrapNav.Link href="/login">
+            <Button>Login</Button>
+        </BootstrapNav.Link>
+        <BootstrapNav.Link href="/register">
+            <Button>Register</Button>
+        </BootstrapNav.Link>
+      </BootstrapNav>
+      <div style={{
+        paddingLeft: "85px"
+      }}>
+      <Row>
+      <Card bg="info" text="white" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Primary Card Title</Card.Title>
+          <Card.Text style={{color : "white"}}>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </Row>
+      </div>
+    </div>
     );
   }
 }
-
 
 export default Admin;
